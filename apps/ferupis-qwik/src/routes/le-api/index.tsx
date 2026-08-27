f3fc686135d1bd029b5e52a4284b92bfe82117ce
Giulio Marinelli <122180544+giuliomarinelli@users.noreply.github.com>
2026-08-27 08:49:38 +0200

    feat(routes): render le api MDX content

--- apps/ferupis-qwik/src/routes/le-api/index.tsx
+++ apps/ferupis-qwik/src/routes/le-api/index.tsx
@@ -1,9 +1,3 @@
-import { component$ } from "@builder.io/qwik";
-import { ContentHeader } from "~/components/content-typography";
+import LeApiPage from "~/components/content/le-api/page.mdx";
 
-export default component$(() => (
-  <ContentHeader.Root>
-    <ContentHeader.Eyebrow text="Ferupis" />
-    <ContentHeader.Heading text="Le Api" tag="h1" />
-  </ContentHeader.Root>
-));
+export default LeApiPage;
