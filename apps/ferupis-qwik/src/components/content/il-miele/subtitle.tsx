b390bb953e7786cee635e8570f1808cf9369cf9e
Giulio Marinelli <122180544+giuliomarinelli@users.noreply.github.com>
2026-08-27 14:19:49 +0200

    feat: add product content pages

---
+++ apps/ferupis-qwik/src/components/content/il-miele/subtitle.tsx
@@ -0,0 +1,7 @@
+import { component$ } from "@builder.io/qwik";
+
+export const IlMieleSubtitle = component$(() => (
+  <p class="mt-5 max-w-xl text-base leading-7 opacity-75 sm:text-lg">
+    Dalla raccolta del nettare alla maturazione nel favo.
+  </p>
+));
