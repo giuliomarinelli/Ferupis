7ff67c09d77dcdddce50539985284b79a9c31812
Giulio Marinelli <122180544+giuliomarinelli@users.noreply.github.com>
2026-08-28 12:56:27 +0200

    feat: add responsive photo gallery

--- apps/ferupis-qwik/src/routes/foto/index.tsx
+++ apps/ferupis-qwik/src/routes/foto/index.tsx
@@ -1,3 +1,19 @@
 import { component$ } from "@builder.io/qwik";
+import { DocumentHead } from "@builder.io/qwik-city";
+import { FotoPage } from "~/components/content/foto/page";
+import { EndPageBtnBlock } from "~/components/UI/end-page-btn-block";
 
-export default component$(() => <>Foto works!</>)
+export default component$(() => (
+  <>
+    <main>
+      <FotoPage />
+    </main>
+    <aside>
+      <EndPageBtnBlock />
+    </aside>
+  </>
+));
+
+export const head: DocumentHead = {
+  title: "Foto | Ferupis",
+};
